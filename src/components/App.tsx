becdb68afb013906af82e1d26bd12967b0a8431a
Administrator <admin@example.com>
2021-02-27 09:49:50 +0900

    add basic features

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Audio from './Audio'
 import '../assets/css/App.css'
 
 const App: React.FC = () => {
-  return <div className="App" />
+  return (
+    <div className="App">
+      <Audio />
+    </div>
+  )
 }
 
 export default App
