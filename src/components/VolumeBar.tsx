0ba2a745342c70a62856787302299a6146d90926
Administrator <admin@example.com>
2021-02-28 15:22:05 +0900

    ボリュームスライダーの見た目修正

--- src/components/VolumeBar.tsx
+++ src/components/VolumeBar.tsx
@@ -3,27 +3,60 @@ import { Grid } from '@material-ui/core'
 import { VolumeUp, VolumeOff } from '@material-ui/icons'
 import styled from 'styled-components'
 
+type StyledProp = {
+  isDispSlider: boolean
+}
+
 const VolumeBar: React.FC = () => {
   const [mute, setMute] = useState(true)
+  const [isDispSlider, setIsDispSlider] = useState(false)
 
+  const dispVolSlider = () => {
+    setIsDispSlider(true)
+  }
+  const noDispVolSlider = () => {
+    setIsDispSlider(false)
+  }
   return (
-    <StyledVolume item xs={10}>
-      <span>{mute ? <VolumeUp /> : <VolumeOff />}</span>
-    </StyledVolume>
+    <Grid item xs={10}>
+      <StyledVolumeArea
+        isDispSlider={isDispSlider}
+        onMouseEnter={dispVolSlider}
+        onFocus={dispVolSlider}
+        onMouseLeave={noDispVolSlider}
+        onBlur={noDispVolSlider}
+      >
+        <button type="button">
+          <VolumeUp />
+        </button>
+        {isDispSlider && <input type="range" />}
+      </StyledVolumeArea>
+    </Grid>
   )
 }
 
 export default VolumeBar
 
-const StyledVolume = styled(Grid)`
-  color: ${(props) => props.theme.colors.primaryText};
+const StyledVolumeArea = styled.div<StyledProp>`
   position: relative;
-  span {
+  width: ${(props) => (props.isDispSlider ? 160 : 30)}px;
+  height: 60px;
+  button {
     position: absolute;
     top: 18px;
+    padding: 0;
+    border: none;
+    outline: none;
+    background-color: transparent;
+    color: ${(props) => props.theme.colors.primaryText};
     cursor: pointer;
     &:hover {
       opacity: 0.8;
     }
+  }
+  input {
+    position: absolute;
+    top: 20px;
+    left: 24px;
   }
 `
