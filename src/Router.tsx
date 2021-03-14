df2ba517b29bdcae5874dc3126be30ea7912ca40
Administrator <admin@example.com>
2021-03-14 21:26:14 +0900

    プロジェクト一覧ページへのルーティング設定

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 import ProjectCreate from './components/ProjectCreate'
 import Main from './components/Main'
+import ProjectsList from './components/ProjectsList'
 
 const Router: React.FC = ({ children }) => {
   return (
@@ -9,6 +10,7 @@ const Router: React.FC = ({ children }) => {
       {children}
       <Switch>
         <Route exact path="/" component={Main} />
+        <Route exact path="/projects" component={ProjectsList} />
         <Route exact path="/project-create" component={ProjectCreate} />
         <Route component={() => <Redirect to="/" />} />
       </Switch>
---
+++ src/page/ProjectsList.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProjectsList: React.FC = () => {
+  return (
+    <div>
+      <h1>projects list page</h1>
+    </div>
+  )
+}
+
+export default ProjectsList
